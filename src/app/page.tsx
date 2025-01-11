d6f6dddd69abfe6dfa8f2c61fd492fb267cb0135
jwo0o0 <jwo0o0358@gmail.com>
2025-01-11 11:27:47 +0900

    feat: front page UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,20 @@
+import Link from 'next/link';
+import Flex from '@/components/Flex';
+import TitleText from '@/components/common/TitleText';
+import Button from '@/components/common/Button';
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <Flex
+      flexCol={true}
+      align="center"
+      justify="center"
+      className="justify-center h-full"
+    >
+      <TitleText text="개발자 프사 생성기" />
+      <div className="w-48 h-48 mt-8 bg-neutral-300" />
+      <Link href="/select/face">
+        <Button text="시작하기" className="pt-6 text-lg" />
+      </Link>
+    </Flex>
+  );
 }
