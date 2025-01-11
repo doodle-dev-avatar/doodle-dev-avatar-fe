8fb6b7a76fd4419db808f74ab8d89e45a72d68e2
jwo0o0 <jwo0o0358@gmail.com>
2025-01-11 11:27:29 +0900

    feat: Footer Component

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+  return (
+    <div
+      className="w-full h-16 px-4 flex flex-col justify-center absolute bottom-0
+    bg-neutral-900 text-neutral-100 font-light text-sm"
+    >
+      <div className="">© 2024 ---. All rights reserved.</div>
+    </div>
+  );
+};
+
+export default Footer;
