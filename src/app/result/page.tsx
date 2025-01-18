c1357d536ed464ec78277ec57690145626c7881b
jwo0o0 <jwo0o0358@gmail.com>
2025-01-18 22:09:44 +0900

    feat: 공유 Button 클릭시 Toast 추가

--- src/app/result/page.tsx
+++ src/app/result/page.tsx
@@ -5,11 +5,26 @@ import TitleText from '@/components/common/TitleText';
 import Button from '@/components/common/Button';
 import { ArrowPathIcon } from '@/components/Icons/ArrowPathIcon';
 import { ShareIcon } from '@/components/Icons/ShareIcon';
+import useCopyToClipboard from '@/hooks/useCopyToClipboard';
+import { useToast } from '@/hooks/use-toast';
+import * as Toast from '@radix-ui/react-toast';
 
 export default function ResultPage() {
   const router = useRouter();
   const handleClickRetry = () => {
     router.push('/');
+  };
+
+  const { toast } = useToast();
+
+  const { copyToClipboard } = useCopyToClipboard();
+  const handleCopyLink = () => {
+    const linkToCopy = window.location.href;
+    copyToClipboard(linkToCopy);
+    toast({
+      description: '링크가 복사되었습니다.',
+      duration: 2000,
+    });
   };
 
   return (
@@ -23,10 +38,12 @@ export default function ResultPage() {
       <div className="w-60 h-60 mt-8 bg-neutral-300" />
       <div className="w-60 mt-8 flex justify-around">
         <Button type="dark">이미지 저장하기</Button>
-        <Button className="items-center">
-          <ShareIcon className="mr-1" />
-          공유
-        </Button>
+        <Toast.Provider duration={3000}>
+          <Button className="items-center" handleClick={handleCopyLink}>
+            <ShareIcon className="mr-1" />
+            공유
+          </Button>
+        </Toast.Provider>
       </div>
       <Button className="m-6 items-center" handleClick={handleClickRetry}>
         다시하기
