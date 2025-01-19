4f11cf017493dbfb76dc58d8c677cac20f43a652
SuJin <kimsujin001127@gmail.com>
2025-01-19 15:12:53 +0900

    feat: type 정의

---
+++ src/components/constants/index.tsx
@@ -0,0 +1,6 @@
+export * from './backgroundIconType';
+export * from './expressionIconType';
+export * from './faceIconType';
+export * from './goodsIconType';
+export * from './propsType';
+export * from './commentIconType';
