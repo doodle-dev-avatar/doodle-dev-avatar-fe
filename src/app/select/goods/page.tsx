98e4f11502c02f87de33365e5f0cd5c3c1905017
SuJin <kimsujin001127@gmail.com>
2025-01-21 18:30:26 +0900

    feat: page layout 설정

--- src/app/select/goods/page.tsx
+++ src/app/select/goods/page.tsx
@@ -1,3 +1,19 @@
+import GoodsImage from '@/components/common/Image/GoodsImage';
+import SlotMachine from '@/components/widgets/SlotMachine';
+
 export default function SelectGoodsPage() {
-  return <>select goods</>;
+  const items = [
+    <GoodsImage key="none" type="none" />,
+    <GoodsImage key="monster" type="monster" />,
+    <GoodsImage key="plant" type="plant" />,
+    <GoodsImage key="coffee" type="coffee" />,
+    <GoodsImage key="books" type="books" />,
+    <GoodsImage key="phone" type="phone" />,
+  ];
+
+  return (
+    <>
+      <SlotMachine items={items} selectType="goods" />
+    </>
+  );
 }
