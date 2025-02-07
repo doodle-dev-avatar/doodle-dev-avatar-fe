22223053cdea2d2a740210d8dba6aea90016a818
김정우 <jwo0o0358@gmail.com>
2025-02-07 22:38:40 +0900

    fix: 선택 결과 전역 상태에서 가져오기

--- src/components/result/Background.tsx
+++ src/components/result/Background.tsx
@@ -3,7 +3,7 @@ import Flex from '../Flex';
 import { useSelectStore } from '@/store/useSelectStore';
 
 export const Background = () => {
-  const background = useSelectStore((state) => state.background) || 'white';
+  const background = useSelectStore((state) => state.background);
 
   return (
     <Flex className="w-60 h-60">
