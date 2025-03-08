45d901769e9be7ad928f02d1b41c7aac29d2d78f
김정우 <jwo0o0358@gmail.com>
2025-03-08 21:02:40 +0900

    Revert "[#30] feat: meta tag, favicon 설정 (#33)" (#34)
    This reverts commit bb68a6d3f520800d2963dd42811c2154474a4ca3.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,19 +6,6 @@ import { Toaster } from '@/components/ui/toaster';
 export const metadata: Metadata = {
   title: '개발자 프로필 생성기',
   description: '나만의 개발자 프로필을 만들어 보세요!',
-  openGraph: {
-    type: 'website',
-    url: 'https://doodle-dev.netlify.app/',
-    title: '개발자 프로필 생성기',
-    description: '나만의 개발자 프로필을 만들어 보세요!',
-    siteName: '개발자 프로필 생성기',
-    images: [{
-      url: './thumbnail.png',
-      width: 1200,
-      height: 630,
-    }],
-    locale: 'ko_KR',
-  },
 };
 
 export default function RootLayout({
