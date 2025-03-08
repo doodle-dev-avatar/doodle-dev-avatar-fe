22cb75cbabab476b555b3f616fffe33edd1cbdb4
SuJin <kimsujin001127@gmail.com>
2025-03-08 21:27:47 +0900

    fix: 이미지에 절대경로 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export const metadata: Metadata = {
     description: '나만의 개발자 프로필을 만들어 보세요!',
     siteName: '개발자 프로필 생성기',
     images: [{
-      url: './thumbnail.png',
+      url: 'https://doodle-dev.netlify.app/thumbnail.png',
       width: 1200,
       height: 630,
     }],
