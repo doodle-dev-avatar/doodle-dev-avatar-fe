85155fbeb7c4fee152f16961c2d611a7c39e2096
김정우 <jwo0o0358@gmail.com>
2025-03-08 20:38:14 +0900

    [#31] iOS, macOS에서 이미지 내보내기시 버그 수정 (#32)
    * feat: html2canvas, svgr 라이브러리 설치
    
    * feat: svgr 설정 추가
    
    * feat: 배경 svg 이미지 추가
    
    * fix: 배경 svg 이미지 import 방식 수정
    
    * fix: 깨지는 이미지 svg 수정
    
    * fix: Comment 위치 조정
    
    * fix: 이미지 내보내기 라이브러리 html2canvas 로 변경
    
    * chore: html-to-image 라이브러리 삭제

--- src/hooks/useSaveImage.ts
+++ src/hooks/useSaveImage.ts
@@ -1,30 +1,41 @@
 import { useRef } from 'react';
-import { toPng } from 'html-to-image';
 import { useToast } from './use-toast';
+import html2canvas from 'html2canvas';
+
+const applyInlineStyles = (element: HTMLElement) => {
+  const computedStyles = window.getComputedStyle(element);
+  for (const key of computedStyles) {
+    element.style.setProperty(key, computedStyles.getPropertyValue(key));
+  }
+};
 
 export const useSaveImage = () => {
   const imageRef = useRef<HTMLDivElement>(null);
   const { toast } = useToast();
 
-  const saveImage = () => {
+  const saveImage = async () => {
     if (!imageRef.current) return;
-
-    toPng(imageRef.current, {
-      cacheBust: true,
-    })
-      .then((dataUrl) => {
-        const link = document.createElement('a');
-        link.href = dataUrl;
-        link.download = 'developer-avatar.png';
-        link.click();
-      })
-      .catch((error) => {
-        console.error('이미지 저장 중 오류 발생:', error);
-        toast({
-          description: '이미지 저장에 실패했습니다.',
-          duration: 2000,
-        });
+    try {
+      applyInlineStyles(imageRef.current);
+      const canvas = await html2canvas(imageRef.current, {
+        useCORS: true,
+        logging: false,
+        width: 240,
+        height: 240,
+        scale: 3,
       });
+      const dataUrl = canvas.toDataURL('/image/png', 1.0);
+      const link = document.createElement('a');
+      link.href = dataUrl;
+      link.download = 'developer-avatar.png';
+      link.click();
+    } catch (error) {
+      console.error(error);
+      toast({
+        description: '이미지 저장에 실패했습니다.',
+        duration: 2000,
+      });
+    }
   };
 
   return { imageRef, saveImage };
